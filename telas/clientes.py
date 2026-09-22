c61e929472347e31e12ba627ea7f2a4ba194e7e9
ThaisAlves02 <thaisgraziele56@gmail.com>
2026-09-22 20:17:53 -0300

    update clientes.py

--- telas/clientes.py
+++ telas/clientes.py
@@ -6,6 +6,7 @@ from tkinter import ttk
 #----------------------------------------------------------
 cor_fundo = "#E9E9E9"
 cor_frame = "#FFFFFF"
+sub_titulo = "#52677F"
 borda_frame = "#e0e0e0"
 cor_botao = "#262753"
 
@@ -28,6 +29,7 @@ def tela_clientes(container):
     subtitulo = ctk.CTkLabel(
         frame_conteudo,
         text="Cadastro de clientes com nome, telefone, endereço, e-mail e CPF.",
+        text_color=sub_titulo,
         font=("Arial", 14)
     )
     subtitulo.pack( 
@@ -36,41 +38,147 @@ def tela_clientes(container):
         pady=0
     )
 
-    botao_novo_cadastro = ctk.CTkButton(
+#----------------------------------------------------------
+# TABELA - NOME, TELEFONE, E-MAIL E AÇÕES
+#----------------------------------------------------------
+    nome = ctk.CTkLabel(
         frame_conteudo,
-        text="Novo",
-        fg_color= "#262753",
-        font=("Arial", 14, "bold"),
-        corner_radius= 8,
+        text="Nome",
+        font=ctk.CTkFont(size=15, weight="bold"),
+        text_color="#52677F"
     )
-    botao_novo_cadastro.pack(
-        anchor="w",
-        padx=32,
-        pady=40
+    nome.place(x=100, y=125)
+   
+    telefone = ctk.CTkLabel(
+        frame_conteudo,
+        text="Telefone",
+        font=ctk.CTkFont(size=15, weight="bold"),
+        text_color="#52677F"
     )
+    telefone.place(x=300, y=125)
+   
+    email = ctk.CTkLabel(
+        frame_conteudo,
+        text="E-mail",
+        font=ctk.CTkFont(size=15, weight="bold"),
+        text_color="#52677F"
+    )
+    email.place(x=530, y=125)
+   
+    acoes = ctk.CTkLabel(
+        frame_conteudo,
+        text="Ações",
+        font=ctk.CTkFont(size=15, weight="bold"),
+        text_color="#52677F"
+    )
+    acoes.place(x=750, y=125)
+   
+#----------------------------------------------------------
+# FUNÇÃO BOTÃO NOVO
+#----------------------------------------------------------
+    def novo_cadastro():
+   
+        frame_cadastro = ctk.CTkFrame(
+        frame_conteudo, 
+        width=500,
+        height=400,
+        fg_color=cor_frame,
+        border_width=1,
+        border_color=borda_frame
+        )
+    
+        frame_cadastro.pack_propagate(False) 
+        
+        frame_cadastro.pack(expand=True, padx=32, pady=(0, 32))
 
-    #----------------------------------------------------------
-    # LISTA DE CLIENTES
-    #----------------------------------------------------------
+        titulo = ctk.CTkLabel(
+            frame_cadastro,
+            text="Cadastrar novo cliente",
+            font=("Arial", 22, "bold")
+        )
+        titulo.pack(padx = 50, pady = (35,25))
 
-    frame_lista_clientes = ctk.CTkFrame(frame_conteudo)
-    frame_lista_clientes.pack(padx=20, pady=15, fill="both", expand=True)
+        entry_nome = ctk.CTkEntry(
+            frame_cadastro,
+            placeholder_text="Nome",
+            border_width=2,
+            width=420,
+            height=40,
+            text_color="black",
+            fg_color="white",
+            border_color="gray",
+        )
+        entry_nome.pack(pady = 8)
 
-    colunas = ("nome", "telefone", "cpf", "email", "endereco", "acoes")
-    tree = ttk.Treeview(frame_lista_clientes, columns=colunas, show="headings", height=10)
+        entry_sobrenome = ctk.CTkEntry(
+            frame_cadastro,
+            placeholder_text="Sobrenome",
+            border_width=2,
+            width=420,
+            height=40,
+            text_color="black",
+            fg_color="white",
+            border_color="gray",
+        )
+        entry_sobrenome.pack(pady = 8)
 
-    tree.heading("nome", text="Nome")
-    tree.heading("telefone", text="Telefone")
-    tree.heading("cpf", text="CPF")
-    tree.heading("email", text="E-mail")
-    tree.heading("endereco", text="Endereço")
-    tree.heading("acoes", text="Ações")
+        entry_telefone = ctk.CTkEntry(
+            frame_cadastro,
+            placeholder_text="(DDD) 99999-9999",
+            border_width=2,
+            width=420,
+            height=40,
+            text_color="black",
+            fg_color="white",
+            border_color="gray",
+        )
+        entry_telefone.pack(pady = 8)
 
-    tree.column("nome", width=150)
-    tree.column("telefone", width=120)
-    tree.column("email", width=180)
-    tree.column("endereco", width=140)
-    tree.column("acoes", width=140)
+        entry_email = ctk.CTkEntry(
+            frame_cadastro,
+            placeholder_text="E-mail (Opcional)",
+            border_width=2,
+            width=420,
+            height=40,
+            text_color="black",
+            fg_color="white",
+            border_color="gray",
+        )
+        entry_email.pack(pady = 8)
 
-    tree.pack(fill="both", expand=True, padx=5, pady=5)
-
+        # Botão Cadastrar
+        cadastrar_botao = ctk.CTkButton(
+            frame_cadastro,
+            text="Cadastrar",
+            font=ctk.CTkFont(size=14, weight="bold"),
+            width=190,
+            height=40
+        )
+        cadastrar_botao.place(x= 57, y=320)
+   
+        # Botão Cancelar
+        cancelar_botao = ctk.CTkButton(
+            frame_cadastro,
+            text="Cancelar",
+            font=ctk.CTkFont(size=14, weight="bold"),
+            width=190,
+            height=40,
+            fg_color="#555555",
+            hover_color="#444444"
+        )
+        cancelar_botao.place(x=252, y=320)
+   
+    # BOTÃO NOVO
+    novo = ctk.CTkButton(
+        container,
+        text="+ Novo",
+        font=ctk.CTkFont(size=15, weight="bold"),
+        width=100,
+        height=40,
+        fg_color="#008C9E",
+        hover_color="#007A89",
+        text_color="white",
+        corner_radius=6,
+        command=novo_cadastro
+    )
+    novo.place(x=750, y=35)
